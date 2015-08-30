05daa67e74a60b1b70b4782ff2535ec6f1fdb68a
hhl <bbyshp@126.com>
2015-08-30 19:44:43 +0800

    fsd

--- shiro-example-chapter7/src/main/java/com/github/zhangkaitao/shiro/chapter7/web/servlet/LoginServlet.java
+++ shiro-example-chapter7/src/main/java/com/github/zhangkaitao/shiro/chapter7/web/servlet/LoginServlet.java
@@ -22,6 +22,7 @@ import java.io.IOException;
 @WebServlet(name = "loginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
 
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(req, resp);
